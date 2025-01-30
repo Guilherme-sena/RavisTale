d05cdf077b9acac1a7d9db0639ba9423c470b529
Guilherme-sena <a.guilhermesena@gmail.com>
2025-01-29 20:32:45 -0400

    Removed Bubble Child Collision

--- Assets/Scripts/PartsController.cs
+++ Assets/Scripts/PartsController.cs
@@ -9,7 +9,9 @@ public class PartsController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other){
         if(other.transform.tag =="Bubble" && other.transform.childCount == 0){
             transform.SetParent(other.transform);
+            transform.gameObject.GetComponent<Collider2D>().enabled = false;
             transform.localPosition = Vector2.zero;
+            transform.localScale = Vector3.one;
         }
     }
 }
